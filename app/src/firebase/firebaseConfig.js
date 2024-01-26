4feb82262211565a72bff6e61278ef0226ea549c
AleksandrKompanetc <moneymacker7@gmail.com>
2024-01-26 20:47:56 +0200

    added firebase

---
+++ app/src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4EsC2HmyOEz9xL9pfzmapNRuCpdE-cVY",
+  authDomain: "chat-quiz-website-88e7a.firebaseapp.com",
+  projectId: "chat-quiz-website-88e7a",
+  storageBucket: "chat-quiz-website-88e7a.appspot.com",
+  messagingSenderId: "147918320515",
+  appId: "1:147918320515:web:c6692373135b23a747fcb6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
